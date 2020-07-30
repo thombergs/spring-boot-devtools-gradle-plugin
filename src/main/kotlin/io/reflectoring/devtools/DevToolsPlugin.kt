cba8c6b152865ab13becb7caf0cbfa9bf72c5fed
Tom Hombergs <tom.hombergs@gmail.com>
2020-07-30 21:54:34 +1000

    first working version with single-depth folder structure for child projects

--- src/main/kotlin/io/reflectoring/devtools/DevToolsPlugin.kt
+++ src/main/kotlin/io/reflectoring/devtools/DevToolsPlugin.kt
@@ -2,14 +2,87 @@ package io.reflectoring.devtools
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.Task
 import org.gradle.api.artifacts.Configuration
+import org.gradle.api.artifacts.ResolvedDependency
 import org.gradle.api.artifacts.UnknownConfigurationException
+import org.gradle.api.tasks.Copy
+import java.io.File
 
 class DevToolsPlugin : Plugin<Project> {
 
 
     override fun apply(project: Project) {
         addDevToolsDependency(project)
+        val reloadConfiguration = addOrCreateConfiguration(project, "reload")
+
+        project.afterEvaluate {
+            val tasks = mutableListOf<Task>()
+            reloadConfiguration.resolve()
+            reloadConfiguration.resolvedConfiguration.firstLevelModuleDependencies.forEach { dependency ->
+                tasks.add(createReloadClassesTaskForModule(project, dependency))
+                tasks.add(createReloadResourcesTaskForModule(project, dependency))
+            }
+            createAggregateReloadTask(project, tasks)
+        }
+
+    }
+
+    private fun createAggregateReloadTask(project: Project, tasks: List<Task>) {
+        val reloadTask = project.tasks.create("reload")
+        for (task in tasks) {
+            reloadTask.dependsOn(task)
+        }
+    }
+
+    /**
+     * Creates a task that compiles the Java files in a given module and then copies them into
+     * the build folder of the main module for Spring Boot Dev Tools to pick up for reload.
+     */
+    private fun createReloadClassesTaskForModule(project: Project, module: ResolvedDependency): Task {
+        val reloadClassesTask = project.tasks.create("reloadClassesFrom${module.moduleName}", Copy::class.java)
+        module.moduleArtifacts.forEach {
+            if (it.type == "jar") {
+                // A lot of assumptions here:
+                // - we assume a single JAR dependency per module
+                // - we assume the JAR file is in the build/libs folder
+                // - we assume classes of the module have been compiled to build/classes
+                // - we assume that Spring Boot dev tools is watching the build/classes folder of this project
+                val classesFolder = File(it.file.parentFile.parentFile, "classes").absolutePath
+                reloadClassesTask.from(classesFolder)
+                reloadClassesTask.into("build/classes")
+            }
+        }
+
+        // we assume that the module and the main module have the "java" plugin applied
+        reloadClassesTask.dependsOn(":${module.module.id.name}:compileJava")
+        reloadClassesTask.dependsOn("compileJava")
+        return reloadClassesTask
+    }
+
+    /**
+     * Creates a task that processes the resources of a given module and then copies them into
+     * the build folder of the main module for Spring Boot Dev Tools to pick up for reload.
+     */
+    private fun createReloadResourcesTaskForModule(project: Project, module: ResolvedDependency): Task {
+        val reloadResourcesTask = project.tasks.create("reloadResourcesFrom${module.moduleName}", Copy::class.java)
+        module.moduleArtifacts.forEach {
+            if (it.file.path.endsWith(".jar")) {
+                // A lot of assumptions here:
+                // - we assume a single JAR dependency per module
+                // - we assume the JAR file is in the build/libs folder
+                // - we assume the resources of the module are available in build/resources
+                // - we assume that Spring Boot dev tools is watching the build/resources folder of this project
+                val classesFolder = File(it.file.parentFile.parentFile, "resources").absolutePath
+                reloadResourcesTask.from(classesFolder)
+                reloadResourcesTask.into("build/resources")
+            }
+        }
+
+        // we assume that the module and the main module have the "java" plugin applied
+        reloadResourcesTask.dependsOn(":${module.module.id.name}:processResources")
+        reloadResourcesTask.dependsOn("processResources")
+        return reloadResourcesTask
     }
 
     /**
@@ -49,3 +122,7 @@ class DevToolsPlugin : Plugin<Project> {
 
 
 }
+
+open class DevToolsPluginExtension(
+        var projects: List<String>
+)
